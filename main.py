3d7f7481a4c61033d3f31365f09990340c87f92c
Phoenix Campbell <Phoenix.Campbell@trojans.dsu.edu>
2024-02-10 16:25:26 -0600

    general cleanup, simplifying

--- main.py
+++ main.py
@@ -83,33 +83,33 @@ plt.title("Correlation Heatmap")
 plt.show()
 # *heatmap
 
-X = data.drop("Made_Purchase", axis=1)
+x = data.drop("Made_Purchase", axis=1)
 y = data["Made_Purchase"]
 # *Splitting data into features and target variable
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42
 )
 # *train-test split
 
-X_train_encoded = pd.get_dummies(X_train)
-X_test_encoded = pd.get_dummies(X_test)
+x_train_encoded = pd.get_dummies(x_train)
+x_test_encoded = pd.get_dummies(x_test)
 # *performing one-hot encoding on categorical variables
 
-missing_cols = set(X_train_encoded.columns) - set(X_test_encoded.columns)
+missing_cols = set(x_train_encoded.columns) - set(x_test_encoded.columns)
 for col in missing_cols:
-    X_test_encoded[col] = 0
-X_test_encoded = X_test_encoded[X_train_encoded.columns]
+    x_test_encoded[col] = 0
+x_test_encoded = x_test_encoded[x_train_encoded.columns]
 # *Ensuring that the columns in the training and testing datasets match after one-hot encoding
 
 imputer = SimpleImputer(strategy="mean")
-X_train_imputed = imputer.fit_transform(X_train_encoded)
+X_train_imputed = imputer.fit_transform(x_train_encoded)
 
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train_imputed, y_train)
 # *train Logistic Regression model
 
-y_pred = model.predict(X_test_encoded)
+y_pred = model.predict(x_test_encoded)
 # *making predictions
 # TODO having a few issues with NaN values and equalizing values
 
@@ -126,7 +126,7 @@ plt.ylabel("Actual")
 plt.title("Confusion Matrix")
 plt.show()
 
-y_prob = model.predict_proba(X_test_encoded)[:, 1]
+y_prob = model.predict_proba(x_test_encoded)[:, 1]
 fpr, tpr, thresholds = roc_curve(y_test, y_prob)
 roc_auc = roc_auc_score(y_test, y_prob)
 plt.plot(fpr, tpr, label="ROC curve (area = %0.2f)" % roc_auc)
@@ -135,6 +135,6 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel("False Positive Rate")
 plt.ylabel("True Positive Rate")
-plt.title("Receiver Operating Characteristic (ROC) Curve")
+plt.title("(ROC) Curve")
 plt.legend(loc="lower right")
 plt.show()
